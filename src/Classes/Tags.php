7a7e68694450e4e78136efbae6263682e2fe2813
Denys Hrechanyi <dhrechanyi@gmail.com>
2019-10-05 17:23:07 +0300

    added Tags class

---
+++ src/Classes/Tags.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Hrechanyi\ActiveCampaign\Classes;
+
+
+class Tags extends Connector
+{
+
+
+	public function get($tag_id)
+	{
+		return $this->request('GET', 'tags/' . strval($tag_id));
+	}
+
+
+	public function all()
+	{
+		return $this->request('GET', 'tags');
+	}
+
+
+	public function create($params)
+	{
+		return $this->request('POST', 'tags', ['tag' => $params]);
+	}
+
+
+	public function update($tag_id, $params)
+	{
+		return $this->request('PUT', 'tags/' . strval($tag_id), ['tag' => $params]);
+	}
+
+
+	public function delete($tag_id)
+	{
+		return $this->request('DELETE', 'tags/' . strval($tag_id));
+	}
+
+
+}
