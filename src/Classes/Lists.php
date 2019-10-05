1bc971e46fe287a66e806e0f88059b604378cd0a
Denys Hrechanyi <dhrechanyi@gmail.com>
2019-10-05 17:05:40 +0300

    added Lists class

---
+++ src/Classes/Lists.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Hrechanyi\ActiveCampaign\Classes;
+
+
+class Lists extends Connector
+{
+
+
+	public function get($list_id)
+	{
+		return $this->request('GET', 'lists/' . strval($list_id));
+	}
+
+
+	public function all()
+	{
+		return $this->request('GET', 'lists');
+	}
+
+
+	public function create($params)
+	{
+		return $this->request('POST', 'lists', ['list' => $params]);
+	}
+
+
+	public function createGroup($params)
+	{
+		return $this->request('POST', 'listGroups', ['listGroup' => $params]);
+	}
+
+
+	public function delete($list_id)
+	{
+		return $this->request('DELETE', 'lists/' . strval($list_id));
+	}
+
+}
